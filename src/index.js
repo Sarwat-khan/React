3bd24996ae5728f5097537e4391f5db8079df9ac
Sarwat-khan <munirsaba10@gmail.com>
2024-09-19 05:25:48 +0500

    Deployed website

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import First from './components/First';
 import Card from './components/card';
 import Calculator from './components/second';
@@ -10,51 +11,28 @@ import createCard from './components/Cards';
 import contacts from './components/contacts';
 import Addintodiary from './components/Addtodiary.jsx';
 
-
-
-
-const router = createBrowserRouter([
-
-    {
-        path: "/",
-        element: <App />,  // Root path should render App component
-      },
-  
-  {
-    path: "/First",
-    element: <First />,
-  },
-
- 
-  {
-    path: "/Calculator",
-    element: <Calculator />,
-  },
- 
-  {
-    path: "/Card",
-    element : <Card 
-        name="Sarwat Khan"
-        img="https://i.pinimg.com/736x/8e/8b/47/8e8b47a9f9ca5c6dbd772608ecfb9d06.jpg"
-        tel="+923443888454"
-        email="sarwatkhan2003@gmail.com"
-      />
-  },
-
-
-  {
-    path: "/Mapping",
-    element: <div><h1>Mapping Practice</h1>{contacts.map(createCard)}</div>
-  },
-
-  {
-    path: "/Addintodiary",
-    element: <Addintodiary/>,
-  }
-  
-]);
-
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 
-root.render(<RouterProvider router={router}> </RouterProvider>);
-
+root.render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} /> 
+      <Route path="/First" element={<First />} />
+      <Route path="/Calculator" element={<Calculator />} />
+      <Route 
+        path="/Card" 
+        element={<Card 
+          name="Sarwat Khan"
+          img="https://i.pinimg.com/736x/8e/8b/47/8e8b47a9f9ca5c6dbd772608ecfb9d06.jpg"
+          tel="+923443888454"
+          email="sarwatkhan2003@gmail.com"
+        />} 
+      />
+      <Route 
+        path="/Mapping" 
+        element={<div><h1>Mapping Practice</h1>{contacts.map(createCard)}</div>} 
+      />
+      <Route path="/Addintodiary" element={<Addintodiary />} />
+    </Routes>
+  </HashRouter>
+);
